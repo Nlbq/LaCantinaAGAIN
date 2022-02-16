4a4fd180cf832770f35dee654fd9b230917ffaba
Nlbq <johndoooe@laposte.net>
2022-02-16 22:49:53 +0100

    fre

---
+++ build/precache-manifest.71654c5b69b5ef1526e801f98e3468de.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92274f12013b886b576358f77fcd6359",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f52912a0a84809d899b0",
+    "url": "/static/css/main.1b927f76.chunk.css"
+  },
+  {
+    "revision": "fc88b62406fa40f54087",
+    "url": "/static/js/2.f565250b.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.f565250b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f52912a0a84809d899b0",
+    "url": "/static/js/main.28da4553.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  },
+  {
+    "revision": "5da1558736a1290e43ef118fa8a4b5ce",
+    "url": "/static/media/SIGHHD.5da15587.jpg"
+  },
+  {
+    "revision": "0800d4862c35e20ed05e01da4eb3b406",
+    "url": "/static/media/boheme.0800d486.jpg"
+  },
+  {
+    "revision": "42a3ddce27ba649c8f4fa17efc3287af",
+    "url": "/static/media/kanagawa.42a3ddce.jpg"
+  },
+  {
+    "revision": "41f258cc779ac0b9d609366e618db6a4",
+    "url": "/static/media/la cantina fond.41f258cc.jpg"
+  },
+  {
+    "revision": "1d1f029b5091b4e6d17e5790f85d1819",
+    "url": "/static/media/lacantinabackground.1d1f029b.jpg"
+  },
+  {
+    "revision": "9cc658e5a1141c4b47d23d437abcd81b",
+    "url": "/static/media/waltz.9cc658e5.jpg"
+  }
+]);
